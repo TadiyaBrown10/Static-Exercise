6cb88f52f0310a5090616e024d774d4a089d722d
Tadiya Brown <tadiyabrown2000@gmail.com>
2023-04-20 15:51:00 -0400

    static exercise complete

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var celcius = TempConverter.FahrenheitToCelsius(24);
+            Console.WriteLine($"Celsius {celcius}");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(55);
+            Console.WriteLine($"fahrenheit {fahrenheit}");
         }
     }
 }
